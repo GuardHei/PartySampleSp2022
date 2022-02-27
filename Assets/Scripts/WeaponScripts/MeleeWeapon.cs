4da276894f4a975e2e84c674d720e5829e1d1e98
Sam Ghahremani <sam.ghahremani8@gmail.com>
2022-02-27 15:16:06 -0800

    moved Basic Weapon outlines to the Main scripts folder, and created a subfolder called WeaponScripts

---
+++ Assets/Scripts/WeaponScripts/MeleeWeapon.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : Weapon
+{
+    
+    public void Attack(int type)
+    {
+        
+    }
+
+    public void Slash()
+    {
+
+    }
+
+    public void Thrust()
+    {
+
+    }
+
+    public void Strike()
+    {
+
+    }
+
+    public void Mental()
+    {
+
+    }
+}
---
+++ Assets/Scripts/WeaponScripts/RangedWeapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedWeapon : Weapon
+{
+    public int ammo;
+    public int reloadSpeed;
+
+    public void Attack(int type)
+    {
+
+    }
+
+    public static void Rapid() //LMB
+    {
+
+    }
+
+    public static void Charged() //RMB
+    {
+
+    }
+
+}
